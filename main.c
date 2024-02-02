7181c4afd3f4629736d117c0fb0a890cf659dae1
GotCoding <156408514+GotCoding@users.noreply.github.com>
2024-02-02 09:44:19 +0000

    eiei

--- main.c
+++ main.c
@@ -10,9 +10,9 @@
 #include "node.h"
 
 int main(int argc, const char * argv[]) {
-    int c=5;
-    struct node a,b,*head ;
-    a.value = c;
+    int z=5;
+    struct node a,b,c,*tail,*head, x;
+    a.value = z;
     a.next=&b;
     head=&a;
     b.value=head->value+3;
@@ -24,15 +24,29 @@ int main(int argc, const char * argv[]) {
     2. Add value(11)
     3. Make next become NULL
  */
+    b.next = &c;
+    head->next->next->value = 11;
+    head->next->next->next = NULL;
+    printf("%d\n", head->next->next->value);
     
 /*  Exercise II
         1. Add 1 more than at the begining!!!!
         2. Add value (2)
+    
         
 */
+    head = &x;
+    x.next = &a;
+    x.value = 2;
+    printf("\n\nx = %d\n", x.value);
+    printf("x = %d\n", head->value);
+    printf("a = %d\n", head->next->value);
+    printf("b = %d\n", head->next->next->value);
+    printf("c = %d\n", head->next->next->next->value);
+
     typedef struct node* NodePtr;
     NodePtr tmp=head; //add temp value to faciliate
-        
+
     /*  Exercise III Use loop to print everything
         int i,n=5;
         for(i=0;i<n;i++){
@@ -40,7 +54,14 @@ int main(int argc, const char * argv[]) {
           // What is missing???
         }
     */
-    
+    int i, n=4;
+    for (i = 0; i < n; i++)
+    {
+        printf("%3d", tmp->value);
+        tmp = tmp->next;
+    }
+    printf("\n");
+
    /*  Exercise IV change to while loop!! (you can use NULL to help)
        
          while(){
@@ -48,16 +69,52 @@ int main(int argc, const char * argv[]) {
            // What is missing???
         }
     */
-    
+    tmp=head;
+    while (tmp != NULL)
+    {
+        printf("%3d", tmp->value);
+        tmp = tmp->next;
+    }
+    printf("\n");
+
+
  /*  Exercise V Use malloc to create all nodes, instead of create a struct!!
          //use a loop to help
           
      */
+    NodePtr newhead, newtmp;
+    newhead = (NodePtr)malloc(sizeof(struct node));
+    newtmp = newhead;
+    int j;
+    for (int i = 0, j = 2; i < 4; i++, j+=3)
+    {
+        if (i == 3)
+            newtmp->next = NULL;
+        else
+            newtmp->next = (NodePtr)malloc(sizeof(struct node));
+        newtmp->value = j;
+        printf("%d\n", newtmp->value);
+        newtmp = newtmp->next;
+    }
 
+    newtmp = newhead;
+    while (newtmp != NULL)
+    {
+        printf("%3d", newtmp->value);
+        newtmp = newtmp->next;
+    }
+    printf("\n");
     /*  Exercise VI Free all node !!
          //use a loop to help
           
      */
+
+    while (newhead)
+    {
+        newtmp = newhead;
+        newhead = newhead->next;
+        free(newtmp);
+    }
     
     return 0;
 }
